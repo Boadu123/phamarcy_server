5ca2534ab681aa5f139e7c962b34a92d466351ac
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2026-07-06 10:38:43 +0000

    Initialization of phamarcy server

---
+++ src/main/java/com/example/phamarcy_server/PhamarcyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.phamarcy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamarcyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamarcyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phamarcy_server/PhamarcyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phamarcy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamarcyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
